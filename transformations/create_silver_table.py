11003636b68830effd8cf698535feed360aebbad
anhollis96 <anhollis96@gmail.com>
2026-01-15 21:41:08 +0000

    modularized transformation functions

--- transformations/create_silver_table.py
+++ transformations/create_silver_table.py
@@ -1,5 +1,6 @@
 from pyspark.sql.functions import *
 from pyspark import pipelines as dp
+from transformation_code.bronze_to_silver_transforms import clean_forces_data, clean_commanders_data
 
 #Create Final silver battles table
 
@@ -14,34 +15,17 @@ def battles_silver():
     forces_bronze=spark.read.table("workspace.civilwar2.forces_bronze")
     commanders_bronze=spark.read.table("workspace.civilwar2.commanders_bronze")
     
-    ####Clean Forces Table###
-    #Filter forces data to include only battles with strength and casualties numbers
-    forces_filtered = forces_bronze.filter((col("strength").isNotNull()) & (col("casualties").isNotNull() | (col("killed").isNotNull()) | (col("wounded").isNotNull()) | (col("killed_wounded").isNotNull()) | (col("missing_captured").isNotNull())))
-
-    #Set null casualty values to zero
-    forces_filtered=forces_filtered.fillna(0,subset=["killed","wounded","killed_wounded","missing_captured"])
-
-    #Clean Casualties field
-    forces_filtered=forces_filtered.withColumn("casualties",when(col("casualties").isNull(),col("killed")+col("wounded")+col("killed_wounded")+col("missing_captured")).otherwise(col("casualties"))).drop("killed","wounded","killed_wounded","missing_captured")
-
-    #Get Union Specific data
-    union_forces=forces_filtered.filter(col("belligerent")=="US")
-    union_forces=union_forces.withColumnRenamed("strength","union_strength").withColumnRenamed("casualties","union_casualties").withColumnRenamed("killed","union_killed").withColumnRenamed("wounded","union_wounded").withColumnRenamed("killed_wounded","union_killed_wounded").withColumnRenamed("missing_captured","union_missing_captured").drop("belligerent")
-
-    #Get Confederate Specific data
-    confederate_forces=forces_filtered.filter(col("belligerent")=="Confederate")
-    confederate_forces=confederate_forces.withColumnRenamed("strength","confederate_strength").withColumnRenamed("casualties","confederate_casualties").withColumnRenamed("killed","confederate_killed").withColumnRenamed("wounded","confederate_wounded").withColumnRenamed("killed_wounded","confederate_killed_wounded").withColumnRenamed("missing_captured","confederate_missing_captured").drop("belligerent")
+    union_forces=clean_forces_data(forces_bronze,"union")
+    confederate_forces=clean_forces_data(forces_bronze,"confederate")
 
     #Combine Forces datasets
     cleaned_forces=confederate_forces.join(union_forces,on="battle_id",how="inner")
 
     ####Clean commanders Table#########
     #Get Union and Confederate commanders separately
-    confederate_commanders=commanders_bronze.filter(col("belligerent")=="Confederate")
-    confederate_commanders=confederate_commanders.withColumnRenamed("name","confederate_commander").drop("belligerent").drop("url")
-    union_commanders=commanders_bronze.filter(col("belligerent")=="US")
-    union_commanders=union_commanders.withColumnRenamed("name","union_commander").drop("belligerent").drop("url")
-
+    union_commanders=clean_commanders_data(commanders_bronze,"union")
+    confederate_commanders=clean_commanders_data(commanders_bronze,"confederate")
+    
     #Combine Commanders Tables
     cleaned_commanders=confederate_commanders.join(union_commanders,on="battle_id",how="inner")
 
